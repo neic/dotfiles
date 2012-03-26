91ab35b0fb33093df78a77b24aa28e3cae82da7d
Mathias Dannesbo <neic@neic.dk>
2012-03-26 17:06:11 +0200

    Removed ede-project

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ede-project-directories (quote ("/home/neic/Dokumenter/HTX/El/Swagway/Software/swagway"))))
+ )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
