ad12dd7c9be71bb5fbe0b6cfd766e9c22faf1752
Mathias Dannesbo <neic@neic.dk>
2014-03-22 15:58:17 +0100

    add -shell-escape TeX-engine variants

--- .emacs.d/ema-latex.el
+++ .emacs.d/ema-latex.el
@@ -1,7 +1,7 @@
 ;;; ema-latex.el -- LaTeX setup
 ;;
 ;; Author: Mathias Dannesbo <neic@neic.dk>
-;; Time-stamp: <2014-03-22 15:57:12 (neic)>
+;; Time-stamp: <2014-03-22 15:57:55 (neic)>
 ;;
 ;; Inspired by prelude-latex.el
 ;; (http://www.emacswiki.org/cgi-bin/wiki/Prelude)
@@ -21,6 +21,23 @@
 ;; make the OS decide what viewer to use
 (setq TeX-view-program-list '(("open" "open %s.pdf")))
 (setq TeX-view-program-selection '((output-pdf "open")))
+
+;; add -shell-escape engine variants
+(setq TeX-engine-alist
+      '((xetex_sh "XeTeX shell escape"
+                  "xetex -file-line-error -shell-escape"
+                  "xelatex -file-line-error -shell-escape"
+                  "xetex")
+        (luatex_sh "LuaTeX shell escape"
+                   "luatex -file-line-error -shell-escape"
+                   "lualatex -file-line-error -shell-escape"
+                   "luatex")))
+
+
+;; and make the corresponding local variable safe.
+;; Warning: Make sure you know what code you are executing
+(setq safe-local-variable-values '((TeX-engine . xelatex_sh)
+                                   (TeX-engine . luatex_sh)))
 
 ;; calc for single line LaTeX math
 (setq calc-embedded-open-formula "^")
