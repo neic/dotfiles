15d8a633c3ea7560ebc63a60c0bbcd117822f5af
Mathias Dannesbo <neic@neic.dk>
2020-11-01 18:51:44 +0100

    Add lots of small utilities to nix

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -2,7 +2,54 @@
 
 {
   environment.systemPackages = with pkgs; [
+        # Applications
+        gnupg
+        pass
+
+        # Spelling
+        aspell
+        aspellDicts.da
+        aspellDicts.en
+        aspellDicts.en-computers
+        languagetool
+
+        # Linters
+        black
+        shellcheck
+
+        # System utils
+        colordiff
+        coreutils
         gitAndTools.gitFull
+        htop
+        ncdu
+        pv
+        pwgen
+        silver-searcher
+        trash-cli
+        tree
+
+        # Network
+        httpie
+        nmap
+        openssh
+        rsync
+        wget
+
+        # Archiveing
+        gzip
+        unrar
+        unzip
+        zstd
+
+        # Media
+        atomicparsley
+        ffmpeg
+        imagemagick
+        youtube-dl
+
+        # Infrastructure CLIs
+        kubectl
     ];
 
   # Auto upgrade nix package and the daemon service.
@@ -11,5 +58,6 @@
 
   programs.zsh.enable = true;
 
+  nixpkgs.config.allowUnfree = true;
   system.stateVersion = 4;
 }
