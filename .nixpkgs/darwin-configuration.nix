8a966434747ffd733e177c6c662900138b128f6a
Mathias Dannesbo (Magenta) <md@magenta.dk>
2023-03-28 17:19:42 +0200

    Use fingerprint for sudo

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -147,6 +147,8 @@
     };
   };
 
+  security.pam.enableSudoTouchIdAuth = true;
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
