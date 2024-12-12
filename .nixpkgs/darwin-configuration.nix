c35bf8b00a769b8ada2acc62f13f59e7dd239d34
Mathias Dannesbo <neic@neic.dk>
2024-12-12 06:40:21 +0100

    Use emacs-mac 29.4
    This is an attempt to fix a fleeting bug where emacs somtimes freezes
    with 100% CPU usage. A bug that sounds like the same is described in
    https://www.reddit.com/r/emacs/comments/1ba3819/comment/ku49lmk/.
    
    I have to use this for some time to see if the comes up again.
    
    All the emacs packaging bugs are tracked on https://docs.google.com/spreadsheets/d/1ZTwuL0W0lKMCu_YrAUiUcCiV9shdHrbszjdDjoKJ918/edit?gid=0#gid=0

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -44,7 +44,34 @@ in
         # Applications
         browserpass
         discord
-        emacs-macport
+        # Install emacs-mac 29.4 from the work branch of Mitsuharu Yamamoto's
+        # repo. This code is combined from parts of default.nix and sources.nix
+        # from
+        # https://github.com/NixOS/nixpkgs/tree/nixos-unstable/pkgs/applications/editors/emacs
+        (callPackage # default.nix
+          (import "${pkgs.path}/pkgs/applications/editors/emacs/make-emacs.nix" { # sources.nix
+            pname = "emacs-mac";
+            version = "29.4";
+            variant = "macport";
+            src = pkgs.fetchFromBitbucket { # New code adapted from sources.nix
+              owner = "mituharu";
+              repo = "emacs-mac";
+              rev = "7cc5e67629363d9e98f65e4e652f83bb4e0ee674";
+              hash = "sha256-Uv0AX0d5JLgxHlBD70OIDOO/ImMA6hH1fs5hCuMxw7c";
+            };
+            meta = {
+              description = "Emacs with macOS-specific patches";
+              license = lib.licenses.gpl3Plus;
+              maintainers = [ ];
+            };
+          }) {
+            # default.nix
+            inherit (pkgs.darwin) sigtool;
+            inherit (pkgs.darwin.apple_sdk.frameworks)
+              Accelerate AppKit Carbon Cocoa GSS ImageCaptureCore ImageIO IOKit
+              OSAKit Quartz QuartzCore WebKit;
+            inherit (pkgs.darwin.apple_sdk_11_0.frameworks) UniformTypeIdentifiers;
+          })
         gnupg
         iterm2
         josm
