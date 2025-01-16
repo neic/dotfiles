cf9927d98a89a14ccdf62c2d09344e1d15ee6dff
Mathias Dannesbo <mathias.dannesbo@scalgo.com>
2025-01-16 12:48:51 +0100

    Pin mac-app-util to hash

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -31,8 +31,10 @@ let
     install -D './bin/kyrat' "$out/bin/kyrat"
     '';
   };
-  mac-app-util-src = builtins.fetchTarball "https://github.com/hraban/mac-app-util/archive/master.tar.gz";
-  mac-app-util = import mac-app-util-src {};
+  mac-app-util = import (builtins.fetchTarball {
+    url= "https://github.com/hraban/mac-app-util/archive/548672d0cb661ce11d08ee8bde92b87d2a75c872.tar.gz";
+    sha256="1w80vjcnaysjlzxsp3v4pxq4yswbjvxs8ann2bk0m7rkjljnzz6m";
+  }) {};
 in
 {
   imports = [
