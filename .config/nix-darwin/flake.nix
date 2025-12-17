1fcf518aa128babbef48718b72ff1aa3b0f73839
Mathias Dannesbo <mathias.dannesbo@scalgo.com>
2025-12-17 11:52:10 +0100

    Fix mac-app-util building
    Workaround for gitlab.common-lisp.net Anubis anti-bot blocking automated fetches
    
    https://github.com/hraban/mac-app-util/issues/39
    
    https://github.com/hraban/cl-nix-lite/compare/v0...r4v3n6101:cl-nix-lite:url-fix

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -5,7 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    mac-app-util.url = "github:hraban/mac-app-util";
+    mac-app-util = {
+      url = "github:hraban/mac-app-util";
+      inputs.cl-nix-lite.url = "github:r4v3n6101/cl-nix-lite/url-fix";
+    };
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, mac-app-util }: {
